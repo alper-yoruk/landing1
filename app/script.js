ce4676b6325daf96e4d92ff6c0ff92251178359e
AlperYoruk <alper.yoruk@arabam.com>
2021-01-18 22:22:53 +0300

    add animated mobile menu

--- app/script.js
+++ app/script.js
@@ -1,15 +1,23 @@
-const btnHamburger = document.querySelector('#btnHamburger');
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener('click', function(){
   if (header.classList.contains('open')){
+    body.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    for (elem of fadeElems) {
+      elem.classList.remove('fade-in');
+      elem.classList.add('fade-out');
+    }
   }else {
+    body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    for (elem of fadeElems) {
+      elem.classList.remove('fade-out');
+      elem.classList.add('fade-in');
+    }
   }
 }); 
